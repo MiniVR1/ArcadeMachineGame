9808dedfc81458a9dc5d05b5072f8ed9cbb10314
LonelyTurnip <aidanhunter26@gmail.com>
2026-07-06 23:18:39 +1000

    Basic moving platform
    A basic moving platform

---
+++ Assets/Scripts/Platformer/MovingPlatform.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float direction = 1.0f; 
+    [SerializeField] private float moveSpeed = 5.0f; 
+
+    private Vector3 position;
+
+    public GameObject Player; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move(){
+        position.x += direction * moveSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("DirectionChange")){
+            direction = -direction;
+        }
+        Debug.Log("Penguins");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Player")){
+            Player.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other){
+        if(other.gameObject.CompareTag("Player")){
+            Player.transform.SetParent(null);
+        }
+    }
+}
