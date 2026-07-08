ebc7b611de83503fe63684f28f7fa35fdbb517c8
MiniVR1 <minivr.vr@gmail.com>
2026-07-08 17:06:38 +1000

    Click inputs working better
    +Now needs to click on the hammer to pick up
    + Can now zoom in and out by clicking on the screen
    + added little popup when hovering pickup items

---
+++ Assets/Scripts/World/InteractionManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionManager : MonoBehaviour
+{
+    public Texture2D grabCursor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        RaycastHit[] hits = Physics.RaycastAll(ray, 10);
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                InteractableObject interactable = hit.collider.GetComponent<InteractableObject>();
+                if (interactable != null)
+                {
+                    Cursor.SetCursor(grabCursor, Vector2.zero, CursorMode.Auto);
+                    
+                    if (Mouse.current.leftButton.wasPressedThisFrame)
+                    {
+                        Debug.Log("Interacting with: " + interactable.gameObject.name);
+                        interactable.OnInteract();
+                    }
+                    return; // only find the first interactable thing and skip resetting the cursor
+                }
+            }
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
