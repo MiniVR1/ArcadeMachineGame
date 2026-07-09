47715e5e399a7bcfb9f9f50504c0199e4f0820a3
MiniVR1 <minivr.vr@gmail.com>
2026-07-09 23:55:55 +1000

    Continued merging into Main Scene
    + Added world aesthetics
    ~ changed camera from cinemachine to builtin scripts

--- Assets/Scripts/UI/Camera_System/SwitchCam.cs
+++ Assets/Scripts/UI/Camera_System/SwitchCam.cs
@@ -1,22 +1,47 @@
+using System;
+using System.Collections;
 using Cinemachine;
 using UnityEngine;
 
 public class SwitchCam : MonoBehaviour
 {
-    public CinemachineVirtualCamera cam1;
-    public CinemachineVirtualCamera cam2;
-    public GameObject Reality_Canvas;
-    public SetsUiElementToSelectOnInteraction script;
+    public CameraPositions[] cameraPositions;
+    private Vector3 cameraStartPos;
+    private Vector3 cameraRotation;
 
-    public void SwitchCam1()
+    public Canvas canvas;
+
+    public void SwitchToCamera(int cameraPos)
     {
-        Reality_Canvas.SetActive(false);
-        CameraManager.SwitchCamera(cam1);
-        script.JumpToElement();
+        StopAllCoroutines();
+        StartCoroutine(ZoomToPosition(cameraPositions[cameraPos].position, cameraPositions[cameraPos].rotation));
+        canvas.enabled = false;
     }
 
-    public void SwitchCam2()
+    private IEnumerator ZoomToPosition(Vector3 position, Vector3 rotation)
     {
-        CameraManager.SwitchCamera(cam2);
+        float time = 0;
+        cameraStartPos = Camera.main.transform.localPosition;
+        cameraRotation = Camera.main.transform.localRotation.eulerAngles;
+        while (time < 1)
+        {
+            Camera.main.transform.localPosition = Vector3.Slerp(cameraStartPos, position, time);
+            Camera.main.transform.localRotation = Quaternion.Euler(Vector3.Slerp(cameraRotation, rotation, time));
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+
+    [Serializable]
+    public class CameraPositions
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+        public CameraPositions(Vector3 position, Vector3 rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+        }
     }
 }
