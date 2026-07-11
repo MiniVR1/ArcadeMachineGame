d8fde047ad4290e4ad88a8e5ec937c549fd2f226
OnHiatus1044 <harveyyeo@hotmail.com>
2026-07-11 22:54:46 +1000

    NewGlitches and level 2 complete

---
+++ Assets/Scripts/Platformer/NewGlitchesManager2.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using UnityEngine;
+
+public class NewGlitchesManager2 : MonoBehaviour
+{
+    public LevelManager levelManager;
+    [Header("Glitch Refs")]
+    public NewGlitches[] level1Glitches;
+    public NewGlitches[] level2Glitches;
+    public NewGlitches[] level3Glitches;
+    public int glitchEnabled = 0; //Reset this to 0 when level up!
+
+  
+
+    void OnEnable()
+    {
+        BaseballHit.Instance.hitTop += ToggleGlitch;
+    }
+
+    void ToggleGlitch()
+    {
+        switch (levelManager.level)
+        {
+            case CurrentLevel.Level1:
+                CycleGlitch(level1Glitches);
+                break;
+            case CurrentLevel.Level2:
+                CycleGlitch(level2Glitches);
+                break;
+            case CurrentLevel.Level3:
+                CycleGlitch(level3Glitches);
+                break;
+        }
+    }
+    
+    void CycleGlitch(NewGlitches[] GlitchArray)
+    {
+        GlitchArray[glitchEnabled].ToggleGlitch();
+        glitchEnabled++;
+        if (glitchEnabled >= GlitchArray.Length)
+        {
+            glitchEnabled = 0;
+
+        }
+    }
+
+}
